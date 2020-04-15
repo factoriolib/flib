c9bad25868d6113f58a678a4d2790e724df5e14b
Optera <yousei.kyoko+github@gmail.com>
2020-04-15 09:47:34 +0200

    added format_version, is_new_version

--- control/migration.lua
+++ control/migration.lua
@@ -2,6 +2,8 @@
 local migration = {}
 
 local string_split = require("__core__.lualib.util").split
+local string_match = string.match
+local string_format = string.format
 
 function migration.compare_versions(v1, v2)
   local v1_split = string_split(v1, ".")
@@ -14,6 +16,38 @@ function migration.compare_versions(v1, v2)
     end
   end
   return false
+end
+
+--- normalizes version strings for easy comparison
+---@param version string
+---@param format string | nil defaults to "%02d.%02d.%02d"
+---@return string | nil
+---@usage ("__flib__.control.migration").format_version("1.10.1234", "%02d.%02d.%04d")
+local default_version_format = "%02d.%02d.%02d"
+local version_pattern = "(%d+).(%d+).(%d+)"
+function migration.format_version(version, format)
+  if version then
+    format = format or default_version_format
+    return string_format(format, string_match(version, version_pattern))
+  end
+  return nil
+end
+
+--- checks if normalized strings of current_version > old_version
+---@param old_version string
+---@param current_version string
+---@param format string | nil defaults to "%02d.%02d.%02d"
+---@return bool | nil
+function migration.is_new_version(old_version, current_version, format)
+  local v1 = migration.format_version(old_version, format)
+  local v2 = migration.format_version(current_version, format)
+  if v1 and v2 then
+    if v2 > v1 then
+      return true
+    end
+    return false
+  end
+  return nil
 end
 
 function migration.run(old, migrations, ...)
