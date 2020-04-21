5ab2f05688fca8138a5e4ab2564836902e331918
nexela <nexela@gmail.com>
2020-04-21 00:23:21 -0400

    Initial

--- .scripts/makedocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Auto Generating with ldoc'
-rm -rf 'flib-docs'
-mkdir -p 'flib-docs'
-cp docs/css/spectre.min.css 'flib-docs/spectre.min.css'
-cp docs/css/spectre-icons.min.css 'flib-docs/spectre-icons.min.css'
-ldoc -ic docs/ldoc-config.ld ./
