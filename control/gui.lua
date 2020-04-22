3ff40ad3a66b8c2a6f07ff467ff44a50f3a42304
raiguard <caleb.heuer@gmail.com>
2020-04-21 22:41:44 -0600

    Create handler_lookup table, better variable names

--- control/gui.lua
+++ control/gui.lua
@@ -3,17 +3,18 @@
 local gui = {}
 
 local event = require("__flib__.control.event")
+local util = require("util")
 
--- locals
 local string_gmatch = string.gmatch
 
-local handler_data = {}
-
--- object
 local handlers = {}
 local templates = {}
-local template_lookup = {}
 
+local template_lookup = {}
+local conditional_handlers = {} -- holds conditional handler data for the event module
+local handler_lookup = {} -- holds GUI handler data for this module
+
+-- table extension functions
 local function extend_table(self, data, do_return)
   for k, v in pairs(data) do
     if (type(v) == "table") then
@@ -28,11 +29,9 @@ local function extend_table(self, data, do_return)
   end
   if do_return then return self end
 end
-
 handlers.extend = extend_table
 templates.extend = extend_table
 
--- generate one-dimensional template lookup table
 local function generate_template_lookup(t, template_string)
   for k,v in pairs(t) do
     if k ~= "extend" and type(v) == "table" then
@@ -46,7 +45,6 @@ local function generate_template_lookup(t, template_string)
   end
 end
 
--- recursively navigate the handlers table to create the events
 local function generate_handlers(t, event_string, event_groups)
   event_groups[#event_groups+1] = event_string
   for k,v in pairs(t) do
@@ -54,13 +52,14 @@ local function generate_handlers(t, event_string, event_groups)
       local new_string = event_string.."."..k
       if type(v) == "function" then
         -- shortcut syntax: key is a defines.events or a custom-input name, value is just the handler
-        handler_data[new_string] = {id=defines.events[k] or k, handler=v, group=table.deepcopy(event_groups)}
+        conditional_handlers[new_string] = {id=defines.events[k] or k, handler=v, group=table.deepcopy(event_groups)}
+        handler_lookup[new_string] = v
       elseif v.handler then
         if not v.id then
           v.id = defines.events[k] or k
         end
         v.group = table.deepcopy(event_groups)
-        handler_data[new_string] = v
+        conditional_handlers[new_string] = v
       else
         generate_handlers(v, new_string, event_groups)
       end
@@ -75,43 +74,49 @@ event.register({"on_init_postprocess", "on_load_postprocess"}, function(e)
   generate_template_lookup(templates, "")
   -- create and register conditional handlers for the GUI events
   generate_handlers(handlers, "gui", {})
-  event.register_conditional(handler_data)
+  event.register_conditional(conditional_handlers, handler_lookup)
 end)
 
--- recursively load a GUI template
-local function recursive_build(parent, t, output, filters, player_index)
+--- @section Events
+
+
+
+--- @section Construction
+
+-- navigate a structure to build a GUI
+local function recursive_build(parent, structure, output, filters, player_index)
   -- load template
-  if t.template then
-    for k,v in pairs(template_lookup[t.template]) do
-      t[k] = t[k] or v
+  if structure.template then
+    for k,v in pairs(template_lookup[structure.template]) do
+      structure[k] = structure[k] or v
     end
   end
   local elem
   -- special logic if this is a tab-and-content
-  if t.type == "tab-and-content" then
+  if structure.type == "tab-and-content" then
     local tab, content
-    output, filters, tab = recursive_build(parent, t.tab, output, filters, player_index)
-    output, filters, content = recursive_build(parent, t.content, output, filters, player_index)
+    output, filters, tab = recursive_build(parent, structure.tab, output, filters, player_index)
+    output, filters, content = recursive_build(parent, structure.content, output, filters, player_index)
     parent.add_tab(tab, content)
   else
     -- create element
-    elem = parent.add(t)
+    elem = parent.add(structure)
     -- apply style modifications
-    if t.style_mods then
-      for k,v in pairs(t.style_mods) do
+    if structure.style_mods then
+      for k,v in pairs(structure.style_mods) do
         elem.style[k] = v
       end
     end
     -- apply modifications
-    if t.mods then
-      for k,v in pairs(t.mods) do
+    if structure.mods then
+      for k,v in pairs(structure.mods) do
         elem[k] = v
       end
     end
     -- register handlers
-    if t.handlers then
+    if structure.handlers then
       local elem_index = elem.index
-      local name = "gui."..t.handlers
+      local name = "gui."..structure.handlers
       local group = event.conditional_event_groups[name]
       if not group then error("Invalid GUI event group: "..name) end
         -- check if this event group was already enabled
@@ -132,12 +137,12 @@ local function recursive_build(parent, t, output, filters, player_index)
         end
     end
     -- add to output table
-    if t.save_as then
+    if structure.save_as then
       -- recursively create tables as needed
       local prev = output
       local prev_key
       local nav
-      for key in string_gmatch(t.save_as, "([^%.]+)") do
+      for key in string_gmatch(structure.save_as, "([^%.]+)") do
         prev = prev_key and prev[prev_key] or prev
         nav = prev[key]
         if nav then
@@ -150,7 +155,7 @@ local function recursive_build(parent, t, output, filters, player_index)
       prev[prev_key] = elem
     end
     -- add children
-    local children = t.children
+    local children = structure.children
     if children then
       for i=1,#children do
         output, filters = recursive_build(elem, children[i], output, filters, player_index)
@@ -160,11 +165,11 @@ local function recursive_build(parent, t, output, filters, player_index)
   return output, filters, elem
 end
 
-function gui.build(parent, templates)
+function gui.build(parent, structures)
   local output = {}
   local filters = {}
-  for i=1,#templates do
-    output, filters = recursive_build(parent, templates[i], output, filters, parent.player_index or parent.player.index)
+  for i=1,#structures do
+    output, filters = recursive_build(parent, structures[i], output, filters, parent.player_index or parent.player.index)
   end
   return output, filters
 end
