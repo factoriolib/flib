b3c025801053e76d47c0f9badd0fd948e313f0ee
nexela <nexela@gmail.com>
2020-04-24 23:14:16 -0400

    2 Spaces are eww

--- util/direction.lua
+++ util/direction.lua
@@ -1,6 +1,6 @@
 --- Functions for working with directions.
--- @module util.Direction
--- @usage local Direction = require('__flib__.util.direction')
+-- @module util.direction
+-- @usage local direction = require('__flib__.util.direction')
 -- @see defines.direction
 
 local Direction = {}
@@ -26,7 +26,7 @@ Direction.southwest = defines.direction.southwest
 -- @tparam defines.direction direction the direction
 -- @treturn defines.direction the opposite direction
 function Direction.opposite(direction)
-    return (direction + 4) % 8
+  return (direction + 4) % 8
 end
 
 --- Returns the next direction.
@@ -35,7 +35,7 @@ end
 -- @tparam[opt=false] boolean eight_way true to get the next direction in 8-way (note: not many prototypes support 8-way)
 -- @treturn defines.direction the next direction
 function Direction.next(direction, eight_way)
-    return (direction + (eight_way and 1 or 2)) % 8
+  return (direction + (eight_way and 1 or 2)) % 8
 end
 
 --- Returns the previous direction.
@@ -44,14 +44,14 @@ end
 -- @tparam[opt=false] boolean eight_way true to get the previous direction in 8-way (note: not many prototypes support 8-way)
 -- @treturn defines.direction the next direction
 function Direction.previous(direction, eight_way)
-    return (direction + (eight_way and -1 or -2)) % 8
+  return (direction + (eight_way and -1 or -2)) % 8
 end
 
 --- Returns an orientation from a direction.
 -- @tparam defines.direction direction
 -- @treturn float
 function Direction.to_orientation(direction)
-    return direction / 8
+  return direction / 8
 end
 
 --- Returns a vector from a direction.
@@ -59,26 +59,26 @@ end
 -- @tparam[opt = 1] number distance
 -- @treturn Position
 function Direction.to_vector(direction, distance)
-    distance = distance or 1
-    local x, y = 0, 0
-    if direction == Direction.north then
-        y = y - distance
-    elseif direction == Direction.northeast then
-        x, y = x + distance, y - distance
-    elseif direction == Direction.east then
-        x = x + distance
-    elseif direction == Direction.southeast then
-        x, y = x + distance, y + distance
-    elseif direction == Direction.south then
-        y = y + distance
-    elseif direction == Direction.southwest then
-        x, y = x - distance, y + distance
-    elseif direction == Direction.west then
-        x = x - distance
-    elseif direction == Direction.northwest then
-        x, y = x - distance, y - distance
-    end
-    return {x = x, y = y}
+  distance = distance or 1
+  local x, y = 0, 0
+  if direction == Direction.north then
+    y = y - distance
+  elseif direction == Direction.northeast then
+    x, y = x + distance, y - distance
+  elseif direction == Direction.east then
+    x = x + distance
+  elseif direction == Direction.southeast then
+    x, y = x + distance, y + distance
+  elseif direction == Direction.south then
+    y = y + distance
+  elseif direction == Direction.southwest then
+    x, y = x - distance, y + distance
+  elseif direction == Direction.west then
+    x = x - distance
+  elseif direction == Direction.northwest then
+    x, y = x - distance, y - distance
+  end
+  return {x = x, y = y}
 end
 
 return Direction
