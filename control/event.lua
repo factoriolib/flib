763f6520b5591bd82733418352b4b3cf72d3fe1a
raiguard <caleb.heuer@gmail.com>
2020-05-09 13:41:55 -0600

    Mostly finish documentation
    Bootstrap event shortcuts are now explicitly defined to allow them to be
    documented more easily.

--- control/event.lua
+++ control/event.lua
@@ -1,28 +1,85 @@
---- @module control.event
--- Syntax sugar for event manipulation.
+--- Syntax sugar for event manipulation.
+-- Along with the list of functions below, this module dynamically generates syntax-shortcuts for all @{defines.events}
+-- events. These shortcuts are only to be used when registering a handler to a single event. To register a handler to
+-- multiple events, use @{event.register}.
+--
+-- To use a shortcut, replace `event.register(defines.events.on_built_entity, handler, filters)` with
+-- `event.on_built_entity(handler, filters)`. You can also deregister the handler using `event.on_built_entity(nil)`.
+-- @module control.event
 -- @usage local event = require("__flib__.control.event")
 local event = {}
 
-local bootstrap_events = {"on_init", "on_load", "on_configuration_changed"}
-
--- TODO: Raiguard - Write docs
-
-for _, name in ipairs(bootstrap_events) do
-  event[name] = function(handler)
-    return script[name](handler)
-  end
-end
-
+-- generate syntax shortcuts
 for name, id in pairs(defines.events) do
   event[name] = function(handler, filters)
     return script.on_event(id, handler, filters)
   end
 end
 
+--- Register or deregister a handler to be run during mod init.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run during mod init
+-- event.on_init(function() log("on_init") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_init(nil)
+function event.on_init(handler)
+  return script.on_init(handler)
+end
+
+--- Register or deregister a handler to be run during mod load.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run during mod load
+-- event.on_load(function() log("on_load") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_load(nil)
+function event.on_load(handler)
+  return script.on_load(handler)
+end
+
+--- Register or deregister a handler to be run when mod configuration changes.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run when mod configuration changes
+-- event.on_configuration_changed(function() log("on_configuration_changed") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_configuration_changed(nil)
+function event.on_configuration_changed(handler)
+  return script.on_configuration_changed(handler)
+end
+
+--- Register or deregister a handler to run every N ticks.
+-- @tparam uint nth_tick
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run every 30 ticks
+-- event.on_nth_tick(30, function(e) log("30th tick!") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_nth_tick(30, nil)
 function event.on_nth_tick(nth_tick, handler)
   return script.on_nth_tick(nth_tick, handler)
 end
 
+-- @section Functions
+
+-- TODO Nexela link EventFilters to https://lua-api.factorio.com/latest/Event-Filters.html
+
+--- Register or deregister a handler to or from an event or group of events.
+-- @tparam EventId|EventId[] ids
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @tparam[opt] EventFilters filters
+-- @usage
+-- -- register a handler to a defines.events event that supports filters
+-- event.register(defines.events.on_built_entity, function(e) log("ghost built!") end, {{filter="ghost"}})
+-- -- register a handler to a custom-input
+-- event.register("my-input", function(e) log("my-input pressed!") end)
+-- -- register a handler to multiple events of different types
+-- event.register({"my-input", defines.events.on_lua_shortcut}, function(e) log("do something!") end)
+-- -- deregister a handler from a single event, if one is registered
+-- event.register("my-input", nil)
+-- -- deregister a handler from multiple events, if one is registered
+-- event.register({"my-input", defines.events.on_lua_shortcut}, nil)
 function event.register(ids, handler, filters)
   if type(ids) ~= "table" then
     ids = {ids}
@@ -38,22 +95,59 @@ function event.register(ids, handler, filters)
   return
 end
 
+--- Generate a new, unique event ID.
+-- @return uint
+-- @usage
+-- -- generate a new event ID
+-- local my_event = event.generate_id()
+-- -- raise that event with custom parameters
+-- event.raise(my_event, {whatever_you_want=true, ...})
 function event.generate_id()
   return script.generate_event_name()
 end
 
+--- Retrieve the handler for an event, if one exists.
+-- @tparam EventId id
+-- @return function The registered handler, or `nil` if one isn't registered.
+-- @usage
+-- local existing_handler = event.get_handler(defines.events.on_gui_click)
 function event.get_handler(id)
   return script.get_event_handler(id)
 end
 
+--- Raise an event as if it were actually called.
+-- @tparam EventId id
+-- @tparam EventData event_data The event data that will be passed to the handlers.
+-- @usage
+-- event.raise(defines.events.on_gui_click, {player_index=e.player_index, element=my_button, ...})
 function event.raise(id, event_data)
   return script.raise_event(id, event_data)
 end
 
+--- Retrieve the mod event order.
+-- @return string
+-- @usage
+-- local event_order = event.get_order()
 function event.get_order()
   return script.get_event_order()
 end
 
+--- Set the filters for the given event(s).
+-- @tparam EventId|EventId[] ids
+-- @tparam EventFilters filters The filters to set, or `nil` to clear the filters.
+-- @usage
+-- -- set the filters for a single event
+-- event.set_filters(defines.events.on_built_entity, {
+--   {filter="ghost"},
+--   {filter="type", type="assembling-machine"}
+-- })
+-- -- set the filters for multiple events that have compatible formats
+-- event.set_filters({defines.events.on_built_entity, defines.events.on_robot_built_entity}, {
+--   {filter="ghost"},
+--   {filter="type", type="assembling-machine"}
+-- })
+-- -- clear event filters if any are set
+-- event.set_filters(defines.events.on_robot_built_entity, nil)
 function event.set_filters(ids, filters)
   if type(ids) ~= "table" then
     ids = {ids}
@@ -64,8 +158,23 @@ function event.set_filters(ids, filters)
   return
 end
 
+--- Retrieve the filters for the given event.
+-- @tparam EventId id
+-- @return EventFilters filters The filters, or `nil` if there are none defined.
+-- @usage
+-- local filters = event.get_filters(defines.events.on_built_entity)
 function event.get_filters(id)
   return script.get_event_filter(id)
 end
+
+-- @section Concepts
+
+--- @Concepts EventId
+-- One of the following:
+-- <ul>
+--   <li>A member of @{defines.events}.</li>
+--   <li>A positive @{uint} corresponding to a custom event ID.</li>
+--   <li>For @{event.register} only - a @{string} corresponding to a custom-input name.</li>
+-- </ul>
 
 return event
