61c59302baaaa4e741982e973a201112b7912b2d
raiguard <caleb.heuer@gmail.com>
2020-09-12 21:55:03 -0600

    Consolidate scripts

--- .scripts/makedocs.sh
+++ .scripts/makedocs.sh
@@ -1,6 +1,7 @@
-echo 'Auto Generating with ldoc'
-rm -rf 'flib-docs'
-mkdir -p 'flib-docs'
-cp docs/css/spectre.min.css 'flib-docs/spectre.min.css'
-cp docs/css/spectre-icons.min.css 'flib-docs/spectre-icons.min.css'
+
+echo "Auto Generating with ldoc"
+rm -rf "flib-docs"
+mkdir -p "flib-docs"
+cp docs/css/spectre.min.css "flib-docs/spectre.min.css"
+cp docs/css/spectre-icons.min.css "flib-docs/spectre-icons.min.css"
 ldoc -ic docs/ldoc-config.ld ./
