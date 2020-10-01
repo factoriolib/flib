c6b463cab0aca7d6fca0b3bf0447ed811d8607a9
raiguard <caleb.heuer@gmail.com>
2020-09-30 19:58:24 -0600

    Style and elem mods

--- gui-new.lua
+++ gui-new.lua
@@ -1,13 +1,76 @@
 local flib_gui = {}
 
--- SPECIAL KEYS
-
 local elem_event_keys = {}
 for key, id in pairs(defines.events) do
   if string.find(key, "gui") then
     elem_event_keys[string.gsub(key, "_gui", "")] = id
   end
 end
+
+local elem_style_keys = {
+  -- gui = {readOnly = true},
+  -- name = {readOnly = true},
+  minimal_width = true,
+  maximal_width = true,
+  minimal_height = true,
+  maximal_height = true,
+  natural_width = true,
+  natural_height = true,
+  top_padding = true,
+  right_padding = true,
+  bottom_padding = true,
+  left_padding = true,
+  top_margin = true,
+  right_margin = true,
+  bottom_margin = true,
+  left_margin = true,
+  horizontal_align = true,
+  vertical_align = true,
+  font_color = true,
+  font = true,
+  top_cell_padding = true,
+  right_cell_padding = true,
+  bottom_cell_padding = true,
+  left_cell_padding = true,
+  horizontally_stretchable = true,
+  vertically_stretchable = true,
+  horizontally_squashable = true,
+  vertically_squashable = true,
+  rich_text_setting = true,
+  hovered_font_color = true,
+  clicked_font_color = true,
+  disabled_font_color = true,
+  pie_progress_color = true,
+  clicked_vertical_offset = true,
+  selected_font_color = true,
+  selected_hovered_font_color = true,
+  selected_clicked_font_color = true,
+  strikethrough_color = true,
+  horizontal_spacing = true,
+  vertical_spacing = true,
+  use_header_filler = true,
+  color = true,
+  -- column_alignments = {readOnly = true},
+  single_line = true,
+  extra_top_padding_when_activated = true,
+  extra_bottom_padding_when_activated = true,
+  extra_left_padding_when_activated = true,
+  extra_right_padding_when_activated = true,
+  extra_top_margin_when_activated = true,
+  extra_bottom_margin_when_activated = true,
+  extra_left_margin_when_activated = true,
+  extra_right_margin_when_activated = true,
+  stretch_image_to_widget_size = true,
+  badge_font = true,
+  badge_horizontal_spacing = true,
+  default_badge_font_color = true,
+  selected_badge_font_color = true,
+  disabled_badge_font_color = true,
+  width = true,
+  height = true,
+  padding = true,
+  margin = true
+}
 
 function flib_gui.init()
   if global.__flib then
@@ -59,22 +122,14 @@ local function recursive_build(root_name, parent, structure, refs, assigned_hand
     for key, value in pairs(structure) do
       if key ~= "type" and key ~= "children" then
         local event_id = elem_event_keys[key]
-        if event_id then
+        if elem_style_keys[key] then
+          elem.style[key] = value
+        elseif event_id then
           flib_gui.add_handler(player_index, elem_index, event_id, value, root_name)
           assigned_handlers[elem_index] = true
+        else
+          elem[key] = value
         end
-      end
-    end
-    -- apply style modifications
-    if structure.style_mods then
-      for k, v in pairs(structure.style_mods) do
-        elem.style[k] = v
-      end
-    end
-    -- apply modifications
-    if structure.elem_mods then
-      for k, v in pairs(structure.elem_mods) do
-        elem[k] = v
       end
     end
     -- add to refs table
