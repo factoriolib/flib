646b8fb9de1bfa627be968e87a25ad004c9f2503
raiguard <caleb.heuer@gmail.com>
2020-10-19 12:21:43 -0600

    Clean up

--- reverse_defines.lua
+++
@@ -1,7 +0,0 @@
-local str = "FLIB DEPRECATION WARNING: reverse_defines was renamed to reverse-defines in v0.3.1. Please update your require paths. The old path will cease to work when Factorio 1.1 is released."
-if __DebugAdapter then
-  __DebugAdapter.breakpoint(str)
-else
-  log(str)
-end
-return require("__flib__.reverse-defines")
