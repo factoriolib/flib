22e4c33b1a47e81e4bcb5d63cdfe56e38536b802
raiguard <caleb.heuer@gmail.com>
2020-11-21 22:08:48 -0700

    Clean up and reorganize gui-beta module

--- gui-beta.lua
+++ gui-beta.lua
@@ -1,13 +1,26 @@
+-- FLIB BETA GUI MODULE
+--
+-- Welcome script explorer! This is a beta version of flib's new GUI module. This code is fairly stable and can be used
+-- in mods, but is not guaranteed to keep working between versions. Use at your own risk!
+
+-- Handlers vs. actions:
+-- There are two kinds of event handling currently present in this module:
+-- - Handlers: defining specific functions to call on specific events for specific GUI elements
+-- - Actions: defining specific sets of data to provide on specific events for specific GUI elements
+-- The primary difference between the two is that `handlers` allows you to have small, localized event handlers to do
+-- specific things, while `actions` encourage the use of a single event structure, where the content of the action
+-- message is used to determine what to do. Both have strengths and weaknesses.
+--
+-- Eventually, one of these methods will be removed in favor of the other. I just haven't decided which is better yet.
+-- Only use one or the other on each element - don't use both at once! It will not work.
+
 local reverse_defines = require("__flib__.reverse-defines")
-local table = require("__flib__.table")
 
 local flib_gui = {}
 
--- FIELDS
+-- `HANDLERS` FUNCTIONS
 
 local handlers = {}
-
--- SETUP FUNCTIONS
 
 function flib_gui.add_handlers(tbl)
   -- if `tbl.handlers` exists, use it, else use the table directly
@@ -24,7 +37,57 @@ function flib_gui.hook_gui_events()
   end
 end
 
--- FUNCTIONS
+-- dispatches the stored handler function for the specific element
+function flib_gui.dispatch(e)
+  local elem = e.element
+  if not elem then return false end
+
+  local mod_tags = elem.tags[script.mod_name]
+  if not mod_tags then return end
+
+  local elem_handlers = mod_tags.flib
+  if not elem_handlers then return end
+
+  local event_name = string.gsub(reverse_defines.events[e.name] or "", "_gui", "")
+  local handler_name = elem_handlers[event_name]
+  if not handler_name then return false end
+
+  local handler = handlers[handler_name]
+  if not handler then return false end
+
+  handler(e)
+
+  return true
+end
+
+-- `ACTIONS` FUNCTIONS
+
+function flib_gui.hook_events(func)
+  for name, id in pairs(defines.events) do
+    if string.find(name, "gui") then
+      script.on_event(id, func)
+    end
+  end
+end
+
+-- retrieves the action message from the element's tags
+function flib_gui.get_action(e)
+  local elem = e.element
+  if not elem then return end
+
+  local mod_tags = elem.tags[script.mod_name]
+  if not mod_tags then return end
+
+  local elem_actions = mod_tags.flib
+  if not elem_actions then return end
+
+  local event_name = string.gsub(reverse_defines.events[e.name] or "", "_gui", "")
+  local action = elem_actions[event_name]
+
+  return action
+end
+
+-- BUILDING AND UPDATING FUNCTIONS
 
 -- navigate a structure to build a GUI
 local function recursive_build(parent, structure, refs)
@@ -50,14 +113,11 @@ local function recursive_build(parent, structure, refs)
   end
   -- element handlers
   if structure.handlers then
-    -- do it this way to preserve any other tags
-    local tags = elem.tags
-    if tags[script.mod_name] then
-      tags[script.mod_name].flib_handlers = structure.handlers
-    else
-      tags[script.mod_name] = {flib_handlers = structure.handlers}
-    end
-    elem.tags = tags
+    flib_gui.update_tags(elem, {flib = structure.handlers})
+  end
+  -- element actions
+  if structure.actions then
+    flib_gui.update_tags(elem, {flib = structure.actions})
   end
   -- element reference
   if structure.ref then
@@ -110,60 +170,6 @@ function flib_gui.build(parent, structures)
   return refs
 end
 
-function flib_gui.dispatch(e)
-  local elem = e.element
-  if not elem then return false end
-
-  local mod_tags = elem.tags[script.mod_name]
-  if not mod_tags then return end
-
-  local elem_handlers = mod_tags.flib_handlers
-  if not elem_handlers then return end
-
-  local event_name = string.gsub(reverse_defines.events[e.name] or "", "_gui", "")
-  local handler_name = elem_handlers[event_name]
-  if not handler_name then return false end
-
-  local handler = handlers[handler_name]
-  if not handler then return false end
-
-  handler(e)
-
-  return true
-end
-
-function flib_gui.get_tags(elem)
-  return elem.tags[script.mod_name] or {}
-end
-
-function flib_gui.set_tags(elem, tags)
-  local elem_tags = elem.tags
-  elem_tags[script.mod_name] = tags
-  elem.tags = elem_tags
-end
-
-function flib_gui.delete_tags(elem)
-  local elem_tags = elem.tags
-  elem_tags[script.mod_name] = nil
-  elem.tags = elem_tags
-end
-
-function flib_gui.update_tags(elem, updates)
-  local elem_tags = elem.tags
-  local existing = elem_tags[script.mod_name]
-
-  if not existing then
-    elem_tags[script.mod_name] = {}
-    existing = elem_tags[script.mod_name]
-  end
-
-  for k, v in pairs(updates) do
-    existing[k] = v
-  end
-
-  elem.tags = elem_tags
-end
-
 local function recursive_update(elem, updates)
   if updates.cb then
     updates.cb(elem)
@@ -212,6 +218,40 @@ end
 
 function flib_gui.update(elem, updates)
   recursive_update(elem, updates)
+end
+
+-- TAGS FUNCTIONS
+
+function flib_gui.get_tags(elem)
+  return elem.tags[script.mod_name] or {}
+end
+
+function flib_gui.set_tags(elem, tags)
+  local elem_tags = elem.tags
+  elem_tags[script.mod_name] = tags
+  elem.tags = elem_tags
+end
+
+function flib_gui.delete_tags(elem)
+  local elem_tags = elem.tags
+  elem_tags[script.mod_name] = nil
+  elem.tags = elem_tags
+end
+
+function flib_gui.update_tags(elem, updates)
+  local elem_tags = elem.tags
+  local existing = elem_tags[script.mod_name]
+
+  if not existing then
+    elem_tags[script.mod_name] = {}
+    existing = elem_tags[script.mod_name]
+  end
+
+  for k, v in pairs(updates) do
+    existing[k] = v
+  end
+
+  elem.tags = elem_tags
 end
 
 return flib_gui
