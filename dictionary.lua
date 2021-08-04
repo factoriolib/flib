ed8cad72625a5347807dff30296b975032e830f5
raiguard <caleb.heuer@gmail.com>
2021-08-03 22:33:21 -0600

    More groundwork for actually doing translations

--- dictionary.lua
+++ dictionary.lua
@@ -1,8 +1,12 @@
+local event = require("__flib__.event")
+
 local flib_dictionary = {}
 
 local inner_separator = "⤬"
 local separator = "⤬⤬⤬"
 local max_depth = 5
+
+local language_finished_event = event.generate_id()
 
 local function kv(key, value)
   return key..inner_separator..value..separator
@@ -26,15 +30,15 @@ function Dictionary:add(key, value)
       local new_level = {"", to_add}
       ref[i] = new_level
       self.ref = new_level
-      self.i = 2
+      self.i = 1
       self.r_i = r_i
     else
       local s_i = self.s_i + 1
       self.s_i = s_i
-      local new_set = {"", self:get_starting_key()}
+      local new_set = {""}
       self.ref = new_set
       self.strings[s_i] = new_set
-      self.i = 2
+      self.i = 1
       self.r_i = 1
     end
   end
@@ -46,7 +50,12 @@ function flib_dictionary.init()
   if not global.__flib then
     global.__flib = {}
   end
-  global.__flib.dictionary = {players = {}, raw = {}, translated = {}}
+  global.__flib.dictionary = {
+    in_process = {},
+    players = {},
+    raw = {},
+    translated = {}
+  }
 end
 
 --- Create a new dictionary.
@@ -59,7 +68,7 @@ function flib_dictionary.new(name, keep_untranslated, initial_contents)
   local self = setmetatable(
     {
       -- Indices
-      i = 2,
+      i = 1,
       r_i = 1,
       s_i = 1,
       -- Internal
@@ -83,12 +92,23 @@ function flib_dictionary.new(name, keep_untranslated, initial_contents)
   return self
 end
 
+-- Add the player to the table and request the translation for their language code
 function flib_dictionary.translate(player)
-  -- TODO:
+  local player_table = global.__flib.dictionary.players[player.index]
+  if player_table then
+    error("Player `"..player.name.."` ["..player.index.."] is already translating!")
+  end
+
+  global.__flib.dictionary.players[player.index] = {
+    state = "get_language",
+    player = player,
+  }
+
+  player.request_translation({"", "FLIB_LOCALE_IDENTIFIER", separator, {"locale-identifier"}})
 end
 
 function flib_dictionary.on_tick(event_data)
-
+  -- TODO:
 end
 
 local dictionary_match_string = kv("^FLIB_DICTIONARY_NAME", "(.-)")
@@ -97,7 +117,8 @@ local dictionary_match_string = kv("^FLIB_DICTIONARY_NAME", "(.-)")
   .."(.*)$"
 
 function flib_dictionary.handle_translation(event_data)
-  if event_data.translated and string.find(event_data.result, "^FLIB_DICTIONARY_TRANSLATION") then
+  if not event_data.translated then return end
+  if string.find(event_data.result, "^FLIB_DICTIONARY_TRANSLATION") then
     local _, _, dict_name, dict_lang, string_index, translation = string.find(
       event_data.result,
       dictionary_match_string
@@ -111,8 +132,8 @@ function flib_dictionary.handle_translation(event_data)
       if not dictionary then return end
       local dict_data = global.__flib.dictionary.raw[dict_name]
 
-      for str in string.gmatch(event_data.result, "(.-)"..separator) do
-        local _, _, key, value = string.find(translation, "^(.-)"..inner_separator.."(.-)$")
+      for str in string.gmatch(translation, "(.-)"..separator) do
+        local _, _, key, value = string.find(str, "^(.-)"..inner_separator.."(.-)$")
         if key then
           -- If `keep_untranslated` is true, then use the key as the value if it failed
           local failed = string.find(value, "Unknown key:")
@@ -127,7 +148,40 @@ function flib_dictionary.handle_translation(event_data)
         end
       end
     end
+  elseif string.find(event_data.result, "^FLIB_LOCALE_IDENTIFIER") then
+    local _, _, language = string.find(event_data.result, "^FLIB_LOCALE_IDENTIFIER"..separator.."(.*)$")
+    if language then
+      local script_data = global.__flib.dictionary
+      local player_table = script_data.players[event_data.player_index]
+      if not player_table then return end
+
+      player_table.language = language
+
+      -- Check if this language is already translated or being translated
+      local dictionaries = script_data.translated[language]
+      if dictionaries then
+        script_data.players[event_data.player_index] = nil
+        event.raise(
+          language_finished_event,
+          {dictionaries = dictionaries, language = language, players = {e.player_index}}
+        )
+        return
+      end
+      local in_process = script_data.in_process[language]
+      if in_process then
+        table.insert(in_process.players, event_data.player_index)
+        player_table.status = "waiting"
+        return
+      end
+
+      -- Start translating this language
+      player_table.status = "translating"
+      player_table.dictionary = next(script_data.raw)
+      player_table.i = 1
+    end
   end
 end
+
+flib_dictionary.language_finished_event = language_finished_event
 
 return flib_dictionary
