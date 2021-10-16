5eb0a8f3f0fee5fd69f723fd21b39772f99ccebb
raiguard <caleb.heuer@gmail.com>
2021-10-15 22:19:36 -0600

    Fix table.find docs (closes #36)

--- table.lua
+++ table.lua
@@ -115,8 +115,8 @@ end
 -- @treturn any|nil The key that matches the value, or `nil` if it was not found.
 -- @usage
 -- local tbl = {"foo", "bar"}
--- local contains_foo = table.search(tbl, "foo") -- true
--- local contains_baz = table.search(tbl, "baz") -- false
+-- local contains_foo = table.find(tbl, "foo") -- true
+-- local contains_baz = table.find(tbl, "baz") -- false
 function flib_table.find(tbl, value)
   for k, v in pairs(tbl) do
     if v == value then
