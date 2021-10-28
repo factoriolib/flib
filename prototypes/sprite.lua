5321b725b0eeefd57765d115021bf693ec102032
raiguard <caleb.heuer@gmail.com>
2021-10-27 19:55:00 -0600

    Add frame action icons

--- prototypes/sprite.lua
+++ prototypes/sprite.lua
@@ -12,3 +12,14 @@ for i, color in ipairs{"black", "white", "red", "orange", "yellow", "green", "cy
   }
 end
 data:extend(indicators)
+
+local fab = "__flib__/graphics/frame-action-icons.png"
+
+data:extend{
+  {type = "sprite", name = "flib_pin_black", filename = fab, position = {0, 0}, size = 32, flags = {"icon"}},
+  {type = "sprite", name = "flib_pin_white", filename = fab, position = {32, 0}, size = 32, flags = {"icon"}},
+  {type = "sprite", name = "flib_pin_disabled", filename = fab, position = {64, 0}, size = 32, flags = {"icon"}},
+  {type = "sprite", name = "flib_settings_black", filename = fab, position = {0, 32}, size = 32, flags = {"icon"}},
+  {type = "sprite", name = "flib_settings_white", filename = fab, position = {32, 32}, size = 32, flags = {"icon"}},
+  {type = "sprite", name = "flib_settings_disabled", filename = fab, position = {64, 32}, size = 32, flags = {"icon"}},
+}
