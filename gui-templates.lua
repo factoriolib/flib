03a3f9f7084bda83272f4538ddb3572dfbac4f94
Caleb Heuer <ch@raiguard.me>
2024-03-29 11:00:02 -0600

    Minor performance improvements

--- gui-templates.lua
+++ gui-templates.lua
@@ -9,7 +9,7 @@ local flib_technology = require("__flib__.technology")
 
 local flib_gui_templates = {}
 
---- Create and return a technology slot.
+--- Create and return a technology slot. `on_click` must be a registered GUI handler through `gui-lite`.
 --- @param parent LuaGuiElement
 --- @param technology LuaTechnology
 --- @param level uint
@@ -17,11 +17,13 @@ local flib_gui_templates = {}
 --- @param on_click GuiElemHandler?
 --- @return LuaGuiElement
 function flib_gui_templates.technology_slot(parent, technology, level, research_state, on_click)
-  local progress = flib_technology.get_research_progress(technology, level)
+  local technology_prototype = technology.prototype
+
+  local is_multilevel = flib_technology.is_multilevel(technology)
 
   local research_state_str = flib_table.find(flib_technology.research_state, research_state)
   local style = "flib_technology_slot_" .. research_state_str
-  if technology.upgrade or flib_technology.is_multilevel(technology) or technology.prototype.level > 1 then
+  if technology.upgrade or is_multilevel or technology_prototype.level > 1 then
     style = style .. "_multilevel"
   end
 
@@ -38,7 +40,7 @@ function flib_gui_templates.technology_slot(parent, technology, level, research_
     .add({ type = "flow", style = "flib_technology_slot_sprite_flow", ignored_by_interaction = true })
     .add({ type = "sprite", style = "flib_technology_slot_sprite", sprite = "technology/" .. technology.name })
 
-  if technology.upgrade or flib_technology.is_multilevel(technology) or technology.prototype.level > 1 then
+  if technology.upgrade or is_multilevel or technology_prototype.level > 1 then
     base.add({
       type = "label",
       name = "level_label",
@@ -47,14 +49,14 @@ function flib_gui_templates.technology_slot(parent, technology, level, research_
       ignored_by_interaction = true,
     })
   end
-  if flib_technology.is_multilevel(technology) then
-    local max_level_str = technology.prototype.max_level == flib_math.max_uint and "[img=infinity]"
-      or tostring(technology.prototype.max_level)
+  if is_multilevel then
+    local max_level = technology_prototype.max_level
+    local max_level_str = max_level == flib_math.max_uint and "[img=infinity]" or tostring(max_level)
     base.add({
       type = "label",
       name = "level_range_label",
       style = "flib_technology_slot_level_range_label_" .. research_state_str,
-      caption = technology.prototype.level .. " - " .. max_level_str,
+      caption = technology_prototype.level .. " - " .. max_level_str,
       ignored_by_interaction = true,
     })
   end
@@ -65,9 +67,10 @@ function flib_gui_templates.technology_slot(parent, technology, level, research_
     ignored_by_interaction = true,
   })
 
-  local ingredients_len = 0
-  for i, ingredient in pairs(technology.research_unit_ingredients) do
-    ingredients_len = i
+  local ingredients = technology.research_unit_ingredients
+  local ingredients_len = #ingredients
+  for i = 1, ingredients_len do
+    local ingredient = ingredients[i]
     ingredients_flow.add({
       type = "sprite",
       style = "flib_technology_slot_ingredient",
@@ -76,6 +79,8 @@ function flib_gui_templates.technology_slot(parent, technology, level, research_
     })
   end
   ingredients_flow.style.horizontal_spacing = flib_math.clamp((68 - 16) / (ingredients_len - 1) - 16, -15, -5)
+
+  local progress = flib_technology.get_research_progress(technology, level)
 
   base.add({
     type = "progressbar",
