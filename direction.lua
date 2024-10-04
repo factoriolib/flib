1ba3f8831c22711811a41c575b5f97336bf42786
Caleb Heuer <ch@raiguard.me>
2024-10-04 16:42:32 -0600

    Update direction module for new direction format

--- direction.lua
+++ direction.lua
@@ -11,28 +11,11 @@ local flib_math = require("__flib__.math")
 --- @class flib_direction
 local flib_direction = {}
 
---- defines.direction.north
-flib_direction.north = defines.direction.north
---- defines.direction.east
-flib_direction.east = defines.direction.east
---- defines.direction.west
-flib_direction.west = defines.direction.west
---- defines.direction.south
-flib_direction.south = defines.direction.south
---- defines.direction.northeast
-flib_direction.northeast = defines.direction.northeast
---- defines.direction.northwest
-flib_direction.northwest = defines.direction.northwest
---- defines.direction.southeast
-flib_direction.southeast = defines.direction.southeast
---- defines.direction.southwest
-flib_direction.southwest = defines.direction.southwest
-
 --- Calculate the opposite direction.
 --- @param direction defines.direction
 --- @return defines.direction
 function flib_direction.opposite(direction)
-  return (direction + 4) % 8 --[[@as defines.direction]]
+  return (direction + 8) % 16 --[[@as defines.direction]]
 end
 
 --- Calculate the next four-way or eight-way direction.
@@ -40,7 +23,7 @@ end
 --- @param eight_way? boolean
 --- @return defines.direction
 function flib_direction.next(direction, eight_way)
-  return (direction + (eight_way and 1 or 2)) % 8 --[[@as defines.direction]]
+  return (direction + (eight_way and 2 or 4)) % 16 --[[@as defines.direction]]
 end
 
 --- Calculate the previous four-way or eight-way direction.
@@ -48,14 +31,14 @@ end
 --- @param eight_way? boolean
 --- @return defines.direction
 function flib_direction.previous(direction, eight_way)
-  return (direction + (eight_way and -1 or -2)) % 8 --[[@as defines.direction]]
+  return (direction + (eight_way and -2 or -4)) % 16 --[[@as defines.direction]]
 end
 
 --- Calculate an orientation from a direction.
 --- @param direction defines.direction
 --- @return RealOrientation
 function flib_direction.to_orientation(direction)
-  return direction / 8 --[[@as RealOrientation]]
+  return direction / 16 --[[@as RealOrientation]]
 end
 
 --- Calculate a vector from a direction.
@@ -65,21 +48,21 @@ end
 function flib_direction.to_vector(direction, distance)
   distance = distance or 1
   local x, y = 0, 0
-  if direction == flib_direction.north then
+  if direction == defines.direction.north then
     y = y - distance
-  elseif direction == flib_direction.northeast then
+  elseif direction == defines.direction.northeast then
     x, y = x + distance, y - distance
-  elseif direction == flib_direction.east then
+  elseif direction == defines.direction.east then
     x = x + distance
-  elseif direction == flib_direction.southeast then
+  elseif direction == defines.direction.southeast then
     x, y = x + distance, y + distance
-  elseif direction == flib_direction.south then
+  elseif direction == defines.direction.south then
     y = y + distance
-  elseif direction == flib_direction.southwest then
+  elseif direction == defines.direction.southwest then
     x, y = x - distance, y + distance
-  elseif direction == flib_direction.west then
+  elseif direction == defines.direction.west then
     x = x - distance
-  elseif direction == flib_direction.northwest then
+  elseif direction == defines.direction.northwest then
     x, y = x - distance, y - distance
   end
   return { x = x, y = y }
@@ -111,7 +94,7 @@ function flib_direction.from_positions(source, target, round)
   local deg = math.deg(math.atan2(target.y - source.y, target.x - source.x))
   local direction = (deg + 90) / 45
   if direction < 0 then
-    direction = direction + 8
+    direction = direction + 16
   end
   if round then
     direction = flib_math.round(direction)
